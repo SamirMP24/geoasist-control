21f3baa33979744d34019550b76f73acd98e7d8e
Jefferson Mamani <70217679@certus.edu.pe>
2026-06-30 01:16:26 -0500

    Fix URL hardcodeada en adminService - intento 2

--- frontend-web/src/services/adminService.js
+++ frontend-web/src/services/adminService.js
@@ -1,24 +1,18 @@
 import axios from "axios";
 
-const API_URL =
-  "http://localhost:3000/api/attendance";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/attendance`;
 
-export const getAttendances =
-  async () => {
+export const getAttendances = async () => {
+  const token = localStorage.getItem("token");
 
-    const token =
-      localStorage.getItem("token");
+  const response = await axios.get(
+    `${API_URL}/all`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+  );
 
-    const response =
-      await axios.get(
-        `${API_URL}/all`,
-        {
-          headers: {
-            Authorization:
-              `Bearer ${token}`
-          }
-        }
-      );
-
-    return response.data;
+  return response.data;
 };
